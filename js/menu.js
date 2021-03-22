21ededaa0f009ecd85f17258a45a7e637485344a
Anton Petrovich <63902821+antonpetrovich13@users.noreply.github.com>
2021-03-22 21:36:38 +0300

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,32 @@
+//ВЫПАДАЮЩЕЕ МЕНЮ
+let user = document.querySelector(".user__wrapper");
+user.addEventListener("click", function (e) {
+	let drop = document.querySelector(".drop-down");
+	drop.classList.toggle("_active");
+});
+
+document.documentElement.addEventListener("click", function (e) {
+	if (!e.target.closest('.user')) {
+		let drop = document.querySelector(".drop-down");
+		drop.classList.remove("_active");
+	}
+});
+
+/*//АНИМАЦИЯ ЗАГРУЗКИ
+let arrow = document.querySelector(".main__slider");
+let load = document.querySelector(".main__load");
+arrow.addEventListener("click", function () {
+	//alert("Hi");
+	load.classList.toggle("_active");
+});*/
+
+//СЛАЙДЕР MAIN
+let swiper = new Swiper('.main__slider', {
+	speed: 700,
+	parallax: true,
+	autoHeight: true,
+	navigation: {
+		nextEl: '.main__next',
+		prevEl: '.main__prev',
+	},
+});
