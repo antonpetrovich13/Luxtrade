e335dbe71bdb08370d229982bf50836edc46bcc3
Anton Petrovich <63902821+antonpetrovich13@users.noreply.github.com>
2021-04-05 19:44:15 +0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -11,6 +11,29 @@ document.documentElement.addEventListener("click", function (e) {
 	}
 });
 
+//НАВИГАЦИЯ
+
+const menuLinks = document.querySelectorAll(".nav__link[data-goto]");
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const goToBlock = document.querySelector(menuLink.dataset.goto);
+			const goToBlockValue = goToBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header').offsetHeight;
+
+			window.scrollTo({
+				top: goToBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+}
+
 
 //СЛАЙДЕР MAIN
 let swiper = new Swiper('.main__slider', {
@@ -104,17 +127,12 @@ link.forEach(function (link) {
 	});
 })*/
 
-
-
-
-
-
-
 /*link.forEach(function (link) {
 	link.addEventListener("mouseout", function () {
 		servicies.classList.remove("_active");
 	});
 })*/
+
 
 //СЛАЙДЕР QUOTES
 let quotes = new Swiper('.quotes__slider', {
@@ -136,3 +154,66 @@ anim.addEventListener("transitionend", function () {
 	let arrow = document.querySelector(".quotes__img");
 	arrow.classList.remove('_active');
 })
+
+
+
+
+// ВАЛИДАЦИЯ ФОРМЫ
+
+document.addEventListener('DOMContentLoaded', function () {
+	const form = document.getElementById('form');
+	form.addEventListener('submit', formSend);
+	async function formSend(e) {
+		e.preventDefault();
+
+		let error = formValidate(form);
+		let formData = new FormData(form);
+
+		if (error === 0) {
+			form.classList.add('_sending');
+			let response = await fetch('sendmail.php', {
+				method: 'POST',
+				body: formData
+			});
+			if (response.ok) {
+				let result = await response.json();
+				alert(result.message);
+				form.reset();
+				form.classList.remove('_sending');
+			} else {
+				alert("Ошибка");
+				form.classList.remove('_sending');
+			}
+		} else {
+			alert('Заполните обязательные поля');
+		}
+	}
+
+	function formValidate(form) {
+		let error = 0;
+		let formReq = document.querySelectorAll('._req');
+
+		for (let index = 0; index < formReq.length; index++) {
+			const input = formReq[index];
+			formRemoveError(input);
+			if (input.value === '') {
+				formAddError(input);
+				error++;
+			}
+		}
+		return error;
+	}
+	function formAddError(input) {
+		input.parentElement.classList.add('_error');
+		input.classList.add('_error');
+	}
+	function formRemoveError(input) {
+		input.parentElement.classList.remove('_error');
+		input.classList.remove('_error');
+	}
+
+	//Функция теста телефона
+	function phoneTest(input) {
+		return !/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im.test(input.value);
+	}
+});
